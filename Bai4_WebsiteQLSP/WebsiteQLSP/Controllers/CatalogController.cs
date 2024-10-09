2458a0948278fc5eaaa50f46f73d097bacbf506e
unknown <dangkhoa141003@gmail.com>
2024-10-09 17:47:13 +0700

    Done Bai4

---
+++ Bai4_WebsiteQLSP/WebsiteQLSP/Controllers/CatalogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteQLSP.Models;
+
+namespace WebsiteQLSP.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly Context _context;
+
+        // Constructor nhận DbContext từ dependency injection
+        public CatalogController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Catalog
+        public IActionResult Index()
+        {
+            var catalogs = _context.Catalogs.ToList();
+            return View("Catalog", catalogs);  // Chỉ định view Catalog.cshtml
+        }
+
+    }
+}
---
+++ Bai4_WebsiteQLSP/WebsiteQLSP/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteQLSP.Models;
+
+namespace WebsiteQLSP.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Context _context;
+
+        // Constructor nhận DbContext từ dependency injection
+        public ProductController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Catalog
+        public IActionResult Index()    
+        {
+            var product = _context.Products.ToList();
+            return View("Product", product);  // Chỉ định view Catalog.cshtml
+        }
+
+    }
+}
